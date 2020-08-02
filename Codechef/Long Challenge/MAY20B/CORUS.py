6ee7b5cfd575b4820b75b55dd7681775cd299f6c
Pratish Katiyar <pratishkatiyar@gmail.com>
2020-08-02 21:45:39 +0530

    Create CORUS.py

---
+++ Codechef/Long Challenge/MAY20B/CORUS.py
@@ -0,0 +1,19 @@
+# cook your dish here
+try:
+    tc=int(input())
+    for _ in range(tc):
+        seq=[0] * 50
+        n,q=map( int,input().split() )
+        s=input()
+        for i in range(n):
+            seq[ord(s[i])-97]+=1
+        #print(seq)    
+        for yu in range(q):
+            var=0
+            z=int(input())
+            for i in range(26):
+                if seq[i]>z:
+                    var+=(seq[i]-z)
+            print(var) 
+except:
+    pass
