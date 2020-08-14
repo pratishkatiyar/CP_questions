8c76886bce4999d6aa0c52d3f1238723f3c0aa7f
Pratish Katiyar <pratishkatiyar@gmail.com>
2020-08-14 22:17:48 +0530

    Create A.cpp

---
+++ Codeforces/Educational_codeforces_round/93/A.cpp
@@ -0,0 +1,37 @@
+#pragma GCC optimize ("-O3")
+#include <bits/stdc++.h>
+using ll = long long int;
+using namespace std;
+#define vfast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define f(i,x,n) for(ll i=x;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define debug(x) cout<<x<<"\n";
+#define lb lower_bound
+#define ub upper_bound
+#define bs binary_search
+#define MOD 1000000007
+
+
+int32_t main()
+{
+    vfast
+    ll t;
+    cin>>t;
+    while(t--){
+    	ll n;
+    	cin>>n;
+    	ll ar[n];
+    	f(i,0,n){
+    		cin>>ar[i];
+    	}
+    	if(ar[0]+ar[1]<=ar[n-1])	cout<<1<<" "<<2<<" "<<n<<endl;
+    	else	debug(-1)
+
+
+    	
+    }
+    
+    return 0;
+}
+ 
