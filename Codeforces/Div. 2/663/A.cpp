e2201fe35298e4efbedae1d237160368a2653275
Pratish Katiyar <pratishkatiyar@gmail.com>
2020-09-01 02:17:53 +0530

    Update A.cpp

--- Codeforces/Div. 2/663/A.cpp
+++ Codeforces/Div. 2/663/A.cpp
@@ -1,6 +1,6 @@
 #pragma GCC optimize ("-O3")
 #include <bits/stdc++.h>
-using ll = long long int;
+using ll = long int;
 using namespace std;
 #define vfast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define f(i,x,n) for(ll i=x;i<n;i++)
