b1716ab399fe138f779cab4cf7b7825cfdc2111c
Pratish Katiyar <pratishkatiyar@gmail.com>
2020-09-17 21:16:53 +0530

    Update SSEC0010.cpp

--- Codechef/SSCC2020/SSEC0010.cpp
+++ Codechef/SSCC2020/SSEC0010.cpp
@@ -27,7 +27,6 @@ int32_t main()
     	else	if(x==2 or x==6)	debug("83 83")
     	else	if(x==3 or x==5)	debug("83 83 69")
     	else	if(x==4)	debug("83 83 69 67")
-    }
-    
+    }    
     return 0;
 }
