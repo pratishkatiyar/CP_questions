dbd03d7486db0750d55e31fbf5c40e9f75cfbd22
Pratish Katiyar <pratishkatiyar@gmail.com>
2020-09-25 14:44:45 +0530

    Update CHFNSWAP.py

--- Codechef/Long Challenge/SEPT20B/CHFNSWAP.py
+++ Codechef/Long Challenge/SEPT20B/CHFNSWAP.py
@@ -14,7 +14,8 @@ try:
     tc=readint()
     for _ in range(tc):        
     	count1=0
-    	x=y=x1=y1=i=j=r=0
+    	x=y=0
+        x1=y1=i=j=r=0
     	n=readint()
     	count1=((n*(n+1))//2)        
     	if count1%2!=0:
