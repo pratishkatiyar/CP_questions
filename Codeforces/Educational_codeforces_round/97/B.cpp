5ca51adbdf238cce94c169b1349a906fda8dc82e
Pratish Katiyar <pratishkatiyar@gmail.com>
2020-10-27 23:16:41 +0530

    Create B.cpp

---
+++ Codeforces/Educational_codeforces_round/97/B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        ll count_one = 0, count_zero = 0;
+        cin >> s;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '1')
+            {
+                int count = 0;
+                while (s[i] == '1')
+                {
+                    count++;
+                    i++;
+                }
+                count_one += count - 1;
+            }
+            else
+            {
+                int count = 0;
+                while (s[i] == '0')
+                {
+                    count++;
+                    i++;
+                }
+                count_zero += count - 1;
+            }
+            i--;
+        }
+        cout << max(count_one, count_zero) << "\n";
+    }
+    return 0;
+}
