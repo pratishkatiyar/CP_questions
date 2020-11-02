7d761c90e0e5fb81ae920cd839e02e048f28ed0e
Pratish Katiyar <pratishkatiyar@gmail.com>
2020-11-02 23:08:55 +0530

    Create B.cpp

---
+++ Codeforces/Div. 2/681/B.cpp
@@ -0,0 +1,37 @@
+
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <array>
+
+using namespace std;
+typedef long long int64;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);cout.tie(0);
+
+  int64 Tc = 1; cin >> Tc;
+  while(Tc--)
+  {
+    int64 A, B;
+    cin >> A >> B;
+    string S; cin >> S;
+    int64 n = S.size();
+    int64 res = 0, cur = -1e9;
+    for(int64 i=0; i<n; ++i)
+    {
+      if(S[i] == '0') continue;
+      int64 tmp = i - cur; --tmp;
+      res += min(A, B*tmp);
+      cur = i;
+    }
+    cout << res << "\n";
+  }
+  return 0;
+}
